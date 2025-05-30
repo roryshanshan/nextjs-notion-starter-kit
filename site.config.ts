cd7f28a3d40083252644769beb2a8dd0ffed0597
roryshanshan <75593842+roryshanshan@users.noreply.github.com>
2025-05-30 15:57:07 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -5,7 +5,6 @@ export default siteConfig({
   rootNotionPageId: '2017582413f5804a9762daa8fd7c6657',
 
   // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
@@ -14,21 +13,8 @@ export default siteConfig({
   description: '这是由 Notion 驱动的星舱产品展示网站',
   domain: 'notion.mobilehouse.asia',
 
-
-  // open graph metadata (optional)
-
-
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
-
   // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
-  defaultPageIcon: https://i.postimg.cc/G3tYDFp6/202504112221048.png,
+  defaultPageIcon: 'https://i.postimg.cc/G3tYDFp6/202504112221048.png',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
@@ -36,32 +22,18 @@ export default siteConfig({
   isPreviewImageSupportEnabled: true,
 
   // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
   isRedisEnabled: false,
 
+  // open graph metadata (optional)
+
+  // social usernames (optional)
+  twitter: 'transitive_bs',
+  github: 'transitive-bullshit',
+  linkedin: 'fisch2',
+
   // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
-  //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
   pageUrlOverrides: null,
 
-  // whether to use the default notion navigation style or a custom one with links to
-  // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
+  // navigation style
   navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
 })
