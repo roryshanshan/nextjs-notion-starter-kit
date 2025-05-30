719443a413f11f943cb3df9d7aa6b3550114e863
roryshanshan <75593842+roryshanshan@users.noreply.github.com>
2025-05-30 12:46:08 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,18 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '2017582413f5804a9762daa8fd7c6657',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Gavin 星舱展示',
+  author: 'Gavin Lee',
+  description: '这是由 Notion 驱动的星舱产品展示网站',
+  domain: 'notion.mobilehouse.asia',
+
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
