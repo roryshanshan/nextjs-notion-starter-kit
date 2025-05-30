bd099682f5d35594bdb2498c190ed0593bfacabc
roryshanshan <75593842+roryshanshan@users.noreply.github.com>
2025-05-30 17:44:47 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -16,6 +16,8 @@ export default withBundleAnalyzer({
       { protocol: 'https', hostname: 'abs.twimg.com' },
       { protocol: 'https', hostname: 'pbs.twimg.com' },
       { protocol: 'https', hostname: 's3.us-west-2.amazonaws.com' }
+      { protocol: 'https', hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com' },
+      { protocol: 'https', hostname: 'i.postimg.cc' }
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
